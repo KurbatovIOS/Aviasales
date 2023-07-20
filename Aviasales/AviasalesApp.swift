3285abeac31e7f2c351c233faa8c23664f80e7fe
Artem Kurbatov <akurbatov.work@gmail.com>
2023-07-20 16:41:28 +0400

    Initial Commit

---
+++ Aviasales/AviasalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviasalesApp.swift
+//  Aviasales
+//
+//  Created by Kurbatov Artem on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AviasalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
